54aeefb0b913fae0e1f81ede265bcd4bde98fa88
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-09 17:12:04 +0600

    1st lecture done

--- mainLogic.py
+++ mainLogic.py
@@ -1,6 +1,27 @@
-print("asif ahmed")
+# print("asif ahmed")
 
-width, height = 400,500
+# width, height = 400,500
 
-print(width)
-print(height)
+# print(width)
+# print(height)
+
+# input from user
+
+# your_name = input('enter your name: ')
+# print("hi " +your_name)
+
+# we can input two number also but it store as a string*****
+
+num1 = input("Enter number 1: ")
+num2 = input("Enter number 2: ")
+
+
+
+print(num1 , num2)
+print(num1 +  num2)
+
+# convert string to integer
+
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+print(num1 + num2)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+width, height = 400,500
+
+print(width)
+print(height)
