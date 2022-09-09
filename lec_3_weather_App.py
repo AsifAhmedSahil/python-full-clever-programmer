e37af55a20428b1fba46dc0ca6289b58adf01be8
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-09 17:48:23 +0600

    3rd lecture weather app

---
+++ lec_3_weather_App.py
@@ -0,0 +1,15 @@
+# weatehr detect using if else operator*****
+
+weather = input("How is the weather? : ")
+
+if weather == "rain":
+    print("please take ☂ ")
+elif weather == "cloud":
+    print("weatehr is ☁ ")
+else:
+    print("sunny weather take sunglass plz")
+
+
+
+
+
