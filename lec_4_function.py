b05aa182746adb95d6507a564db1a6016b106404
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-10 12:27:06 +0600

    4th lecture function part 1

---
+++ lec_4_function.py
@@ -0,0 +1,16 @@
+def name():
+    print('sahil')
+
+name()
+
+#dynamic function
+def name(name):
+    print(name)
+
+name('sahil')
+
+# passing two parameters
+def greeting(greet , name):
+    print(f'{greet} {name}!')
+
+greeting("hi" , "sahil")
