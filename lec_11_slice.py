31ff24e0378406492929d2b6b9ed7dcc4047bc11
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-10 16:42:10 +0600

    list -slice-reverse array -method

---
+++ lec_11_slice.py
@@ -0,0 +1,11 @@
+# slicing is fruits one inclusive and second one is exclusive
+
+fruits = ['🍌', '🍓' , '🥭']
+
+print(fruits[0:2])
+
+print(len(fruits))
+
+print(fruits[0:len(fruits)-1])
+
+print(fruits[::-1])
