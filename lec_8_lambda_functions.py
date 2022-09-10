a60f17e4f87f5538621638da4c28c542f49ac359
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-10 15:09:50 +0600

    lambda-function-lec-8

---
+++ lec_8_lambda_functions.py
@@ -0,0 +1,11 @@
+# lambda function == > anonymous function ==> ome line function***
+# implicit return
+
+# lambda arguments : expression 
+
+sum = lambda a,b : a+b 
+
+print(sum(1,5))
+
+greet = lambda greet , name : f'{greet} {name}'
+print(greet('hi' , 'sahil'))
