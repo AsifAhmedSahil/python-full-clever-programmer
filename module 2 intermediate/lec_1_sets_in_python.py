d74d9608fcf9a433b361db19519d89db7e227f45
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-09-15 22:17:12 +0600

    sets in python -module 2 lec-1

---
+++ module 2 intermediate/lec_1_sets_in_python.py
@@ -0,0 +1,13 @@
+from tkinter import RIGHT
+from turtle import right
+
+
+# SETS is used for getting unique stuff or elect unique elemenet selected
+
+list1 = ["python", "Go","C++" , "javaScript"]
+list2 = ["c","SQL" , "python","javaScript"]
+
+language = set(list1+list2)
+
+print(language)
+
